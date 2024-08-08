4f58348ed17803b6cb05fc9af242eff0bae2a845
Elga <Elgaabraham2000@gmail.com>
2024-08-08 10:19:54 +0530

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+const String domain='https://api.openweathermap.org/data/2.5/weather?';
+const String apiKey='831ef6d153702ffe60d32412a4ae6d85';
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'homescreen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:Homescreen(),
+    theme:ThemeData(
+      primaryColor: Colors.white,
+
+    ),
+  ),
+  );
+}
